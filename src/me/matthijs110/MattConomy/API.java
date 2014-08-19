11d899ab34d55fa603ac3721c025decb01a0d016
Amir Omidi <amir+github@aaomidi.com>
2014-08-19 21:45:33 +0430

    Update API.java
    Don't send messages when you're making an API. Beats the whole point of an API.

--- src/me/matthijs110/MattConomy/API.java
+++ src/me/matthijs110/MattConomy/API.java
@@ -12,32 +12,31 @@ public class API {
 	
 	public static void giveMoney(Player p, int i) {
 		Main.config.set(Main.getUUID(p) + ".Balance", Main.config.getInt(Main.getUUID(p) + ".Balance", 0) + i);
-		
 		Main.plugin.saveConfig();
-	
-		p.sendMessage(ChatColor.GREEN + "+ " + ChatColor.GOLD + "$" + i + ".00");
-		p.sendMessage(ChatColor.GREEN + "Your new balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
+		// p.sendMessage(ChatColor.GREEN + "+ " + ChatColor.GOLD + "$" + i + ".00");
+		// p.sendMessage(ChatColor.GREEN + "Your new balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
 	}
 
-	public static void takeMoney(Player p, int i) {
+	public static boolean takeMoney(Player p, int i) {
 		int amount = CurrentBalance(p) - i;
 		int set;
 		if(amount <= 0) {
 			set = 0;
 		} else {
 			set = amount;
-			p.sendMessage(ChatColor.RED + "You can't have less then " + ChatColor.YELLOW + "$0.00");
+			// p.sendMessage(ChatColor.RED + "You can't have less then " + ChatColor.YELLOW + "$0.00");
+		return false;
 		}
 		
 		Main.config.set(Main.getUUID(p) + ".Balance", set);
 		Main.plugin.saveConfig();
-		
-		
-		p.sendMessage(ChatColor.RED + "- " + ChatColor.GOLD + "$" + i + ".00");
-		p.sendMessage(ChatColor.GREEN + "Your new balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
+		return true;
+		// p.sendMessage(ChatColor.RED + "- " + ChatColor.GOLD + "$" + i + ".00");
+		// p.sendMessage(ChatColor.GREEN + "Your new balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
 	}
 	
-	public static void getBalance(Player p) {
-		p.sendMessage(ChatColor.GOLD + "Your current balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
+	public static int getBalance(Player p) {
+		//p.sendMessage(ChatColor.GOLD + "Your current balance is " + ChatColor.YELLOW + "$" + CurrentBalance(p) + ".00");
+		return CurrentBalance(p);
 	}
 }
